8333c5b6f42b01cccf5a7e5e4e312cb69e6308b1
liming <limiang929@gmail.com>
2025-10-23 18:22:50 +0800

    update txdb check function

--- tests/testthat/test-txdb.R
+++ tests/testthat/test-txdb.R
@@ -1,7 +1,22 @@
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 library(ChIPseeker)
+library(yulab.utils)
 
 context("TXDB")
+
+test_that("Update txdb", {
+
+    hg19_txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+    ChIPseeker:::.ChIPseekerEnv(hg19_txdb)
+    expect_equal(ChIPseeker:::get_env_genome(), "hg19")
+
+    hg38_txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
+    ChIPseeker:::.ChIPseekerEnv(hg38_txdb)
+    expect_equal(ChIPseeker:::get_env_genome(), "hg38")
+
+})
+
 
 test_that("txdb", {
     txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
@@ -10,4 +25,3 @@ test_that("txdb", {
     expect_equal(ChIPseeker:::TXID2EG("70455"), "uc002qsd.4/1")
     expect_equal(ChIPseeker:::TXID2EG("70455", geneIdOnly=TRUE), "1")
 })
-
